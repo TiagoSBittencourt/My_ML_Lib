0b82e6a9554826dad272e439efd6e1d5aa40ef73
Tiago <tiago.bittencourt.2005@gmail.com>
2024-10-02 11:20:49 -0300

    Lambda Regularization

--- Code/Multiple_Regression.py
+++ Code/Multiple_Regression.py
@@ -16,7 +16,7 @@ def compute_cost(X, y, w, b):
     cost = np.sum((f_wb - y) ** 2) / (2 * m)   # Mean of sqaured error 
     return cost
 
-def compute_gradient(X, y, w, b): 
+def compute_gradient(X, y, w, b, lambda_=0.001): 
     """
     Compute gradient (Mutiple Variable Regression)
     
@@ -33,12 +33,12 @@ def compute_gradient(X, y, w, b):
     f_wb = np.dot(X, w) + b     # Prediction for each row
     error = f_wb - y            # Error in predictions (derivative)
 
-    dj_dw = np.dot(X.T, error) / m  # Gradient for weights (derivative)
-    dj_db = np.sum(error) / m       # Gradient for bias (derivative)
+    dj_dw = (1/m) * np.dot(X.T, error) + (lambda_ * w / m) # Gradient for weights (derivative) + Regularization
+    dj_db = (1/m) * np.sum(error)                          # Gradient for bias (derivative)
     
     return dj_dw, dj_db
 
-def gradient_descent(X, y, w, b, alpha=0.0001, iters=1000, cost_function=compute_cost, gradient_function=compute_gradient, history_print=False): 
+def gradient_descent(X, y, w, b, alpha=0.0001, lambda_=0.001, iters=1000, cost_func=compute_cost, gradient_func=compute_gradient, history_print=False): 
     """
     Compute gradient descent (Mutiple Variable Regression)
     Performs gradient descent to optimize the parameters w and b. 
@@ -64,14 +64,14 @@ def gradient_descent(X, y, w, b, alpha=0.0001, iters=1000, cost_function=compute
     weights_history = []
 
     for i in range(iters):
-        dj_dw, dj_db = gradient_function(X, y, w, b)
+        dj_dw, dj_db = gradient_func(X, y, w, b, lambda_)
         
         w -= alpha * dj_dw  # Update weight
         b -= alpha * dj_db  # Update bias
 
         # Store history if requested
         if history_print and (i % (iters // 10) == 0 or i == iters - 1):
-            cost = cost_function(X, y, w, b)
+            cost = cost_func(X, y, w, b)
             cost_history.append(cost)
             weights_history.append((w.copy(), b))
             print(f"Iteration {i}: Cost {cost:.2e}, w: {[round(x, 3) for x in w]}, b: {b:.3e}")
